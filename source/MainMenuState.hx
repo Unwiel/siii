b14192d6000d0812026291c64587fc8ea34b8634
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2023-01-18 17:44:55 -0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -124,10 +124,10 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(-12, FlxG.height - 64, FlxG.width, "Vs Saw v" + vsSawVersion, 12);
+		var versionShit:FlxText = new FlxText(-12, FlxG.height - 64, FlxG.width, "Lmao v" + vsSawVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
+		//add(versionShit);
 
 		var versionShit:FlxText = new FlxText(-12, FlxG.height - 44, FlxG.width, "Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
